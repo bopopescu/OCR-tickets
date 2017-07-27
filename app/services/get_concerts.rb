d33f4eb4aa51d00f822a3d3fd2a54fd1c32b2f96
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-26 23:16:37 -0700

    api call wip to get right data...

--- app/services/get_concerts.rb
+++ app/services/get_concerts.rb
@@ -1,5 +1,31 @@
 class Concert
   def self.get_concerts_list_by_artist(artist)
     response = RestClient::Request.execute(method: :get, url: "https://api.setlist.fm/rest/1.0/search/setlists?artistName=#{artist}", headers: {x_api_key: ENV['x_api_key'], Accept: 'application/json'})
+    # response["setlist"][0]["sets"]["set"]
+    # binding.pry
   end
 end
+
+
+#mainset
+# response["setlist"][0]["sets"]["set"][0]
+# {"song"=>
+#   [{"name"=>"Money"},
+#    {"name"=>"Light"},
+#    {"name"=>"Ultra"},
+#    {"name"=>"Rebels in Kontrol"},
+#    {"name"=>"Tohuvabohu"},
+#    {"name"=>"Shake the Cage"},
+#    {"name"=>"Son of a Gun"},
+#    {"name"=>"Last Things"},
+#    {"name"=>"Brainwashed"},
+#    {"name"=>"Animal Out"},
+#    {"name"=>"Salvation"},
+#    {"name"=>"Megalomaniac"},
+#    {"name"=>"WWIII"},
+#    {"name"=>"Amnesia"},
+#    {"name"=>"Hau Ruck"}]}
+
+# encore:
+# response["setlist"][0]["sets"]["set"][1]
+# {"encore"=>1, "song"=>[{"name"=>"A Drug Against War"}, {"name"=>"Adios"}, {"name"=>"Godlike"}]}
--- spec/services/get_setlists_spec.rb
+++ spec/services/get_setlists_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Concert, :vcr => true do
+describe Concert do
   it "returns a 200 success header when the API call is made" do
     band = "KMFDM"
     response = Concert.get_concerts_list_by_artist(band)
@@ -10,6 +10,6 @@ describe Concert, :vcr => true do
   it "returns setlists when the API call is made" do
     band = "KMFDM"
     response = JSON.parse(Concert.get_concerts_list_by_artist(band))
-    expect(response["setlist"][0]["url"]).to_not(eq(nil))
+    expect(response["setlist"][0]["sets"]["set"]).to_not(eq(nil))
   end
 end
