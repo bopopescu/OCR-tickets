9d71a209bad1b8be476a2ae3bd833dfe0a5dfeae
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-26 22:02:02 -0700

    adds model specs for tickets

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,8 @@
-# require 'rails_helper'
-#
-# describe Ticket do
-#   it { should validate_presence_of :attachment }
-# end
+require 'rails_helper'
+
+describe Ticket do
+  it { should validate_presence_of :band }
+  it { should validate_presence_of :venue }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :date }
+end
